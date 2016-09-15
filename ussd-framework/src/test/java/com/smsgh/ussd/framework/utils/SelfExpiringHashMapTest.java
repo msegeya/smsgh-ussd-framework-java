78be1c44a988c463864daffcedabbb1296cb2c94
Aaron <aaron@smsgh.com>
2016-09-15 16:09:40 +0000

    Prepared project for Github deployment.

--- ussd-framework/src/test/java/com/smsgh/ussd/framework/utils/SelfExpiringHashMapTest.java
+++ ussd-framework/src/test/java/com/smsgh/ussd/framework/utils/SelfExpiringHashMapTest.java
@@ -14,64 +14,6 @@ import org.junit.Test;
 public class SelfExpiringHashMapTest {
 
     private final static int SLEEP_MULTIPLIER = 10;
-    
-    @Test
-    public void logInternals() throws InterruptedException {
-        SelfExpiringHashMap<String, String> map = new
-            SelfExpiringHashMap<String, String>();
-        // Expecting no entries in all three internal collections.
-        showInternals(map);
-        
-        map.put("fruit", "banana", 2000);
-        // Expecting single entry in all three internal collections.
-        showInternals(map);
-        
-        map.put("cat", "lion", 1000);
-        // Expecting two entries in all three internal collections.
-        // Expecting "cat" before "fruit" in delayedQueue even though it was
-        // inserted after "fruit".
-        showInternals(map);
-        
-        assertEquals("banana", map.get("fruit"));
-        // Expecting two entries in all three internal collections.
-        // Expecting "cat" before "fruit".
-        showInternals(map);
-        
-        map.put("country", "Ghana", 1000);
-        // Expecting three entries in all three internal collections.
-        // Expecting "cat", "country", "fruit" in that order.
-        // FAILED! Was rather "cat", "fruit", "country".
-        showInternals(map);
-        
-        assertEquals("lion", map.get("cat"));
-        // Expecting three entries in all three internal collections.
-        // Expecting "country", "cat", "fruit" in that order.
-        // FAILED! Was rather "cat", "fruit", "country".
-        showInternals(map);        
-        
-        Thread.sleep(1000);
-        map.remove("cat");
-        // Expecting single entry of "fruit" in all three internal
-        // collections.        
-        showInternals(map);
-        
-        assertNull(map.get("cat"));
-        assertNull(map.get("country"));
-        // Expecting single entry of "fruit" in all three internal
-        // collections.
-        showInternals(map);
-        
-        Thread.sleep(2000);
-        assertNull(map.get("fruit"));
-        // Expecting all three internal collections to be empty.
-        showInternals(map);
-    }
-    
-    private void showInternals(SelfExpiringHashMap<String, String> map) {
-        System.out.format("internalMap: %s\n", map.internalMap);
-        System.out.format("delayQueue: %s\n", map.delayQueue);
-        System.out.format("expiringKeys: %s\n\n", map.expiringKeys);
-    }
 
     @Test
     public void basicGetTest() throws InterruptedException {
